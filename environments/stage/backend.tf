879c3637b00b5d603420a5a3ff7f925fb24683ed
sbodia <havrylets.bohdan.softs@gmail.com>
2023-11-27 16:12:15 +0200

    Change the authorization type

--- environments/stage/backend.tf
+++ environments/stage/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    credentials = "credentials.json"
+    # credentials = "credentials.json"
     bucket      = "blue-schedule"
     prefix      = "terraform/schedule-stage-gitops"
   }
